fda3f853187dae9a360c23a0c835298d9b9b1e50
Jonathan Diego Rodríguez <jon_eldiablo@hotmail.com>
2020-09-11 18:14:07 -0500

    cors =/

--- index.js
+++ index.js
@@ -1,48 +1,3 @@
-/* // server.js
-var jsonServer = require("json-server");
-var server = jsonServer.create();
-var router = jsonServer.router("./DB.json");
-var middlewares = jsonServer.defaults();
-server.use(function(req, res, next) {
-  console.log(req.method);
-  if (req.method === "POST") {
-var qs = require("querystring");
-var body = "";
-req.on("data", function(data) {
-body += data;
-});
-req.on("end", function() {
-var POST = qs.parse(body);
-console.log(POST);
-});
-} else if (req.method === "GET") {
-var url = require("url");
-var urlParts = url.parse(req.url, true);
-console.log(urlParts.query);
-}
-    next();
-});
-server.use(middlewares);
-server.use(router);
-server.listen(3000, function() {
-    console.log("JSON Server is running");
-});
-router.render = function(req, res) {
-    //console.log( req.url );
-    //console.log( router.db.__wrapped__.inventario.length );
-    if (req.url.indexOf("/inventario") === 0) {
-        var records = router.db.__wrapped__.inventario.length;
-        res.jsonp({
-            rows: res.locals.data,
-            total: Math.ceil(records / res.locals.data.length),
-            page: req._page,
-            records: records
-        });
-    } else {
-        res.jsonp(res.locals.data);
-    }
-}; */
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -50,7 +5,7 @@ const db = require('./DB.json');
 const app = express();
 const port = 3000;
 
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors());
 app.use(bodyParser.json())
 
 app.post('/inventario', (req, res) => {
